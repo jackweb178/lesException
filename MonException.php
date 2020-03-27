bf5e54e6d8f751688671d3adde8a0a719f3ea128
talibadio <jackweb178@gmail.com>
2020-03-27 20:12:18 +0000

    Convertir les erreurs en exception

--- MonException.php
+++ MonException.php
@@ -1,22 +1,32 @@
 <?php
 
-//https://openclassrooms.com/fr/courses/1665806-programmez-en-oriente-objet-en-php/1667289-les-exceptions#/id/r-2649412
-
-function additionner($a,$b)
+//https://openclassrooms.com/fr/courses/1665806-programmez-en-oriente-objet-en-php/1667289-les-exceptions#/id/r-1670513
+class MonException extends  ErrorException
 {
-    if (!is_numeric($a) || !is_numeric($b))
-    {   //on lance une exception "MonException"
-        throw  new InvalidArgumentException("Les deux parametre doivent etre des nombre ");
+    public function __toString()
+    {
+        switch ($this->severity)
+        {
+            case E_USER_ERROR ://si l'utilisateur emet une erreur fatal
+                $type="Erreur fatal";
+                break;
+
+            case E_WARNING : //si php emet une alerte
+                $type='Attention';
+                break;
+
+            case E_NOTICE : //si php emet une notice
+            case  E_USER_NOTICE : //si l'itulisateur emet une notic
+                $type='Note';
+                break;
+        }
+        return '<strong>'.$type .'</strong> : [' .$this->code .']'.$this->message . '<br/> <strong>'.$this->file.'</strong> a la ligne <strong>'. $this->line .' </strong>';
     }
-    return $a+$b;
 }
 
-try {
-    echo additionner(12,3) ,'<br/>';
-    echo additionner('as',54),'<br/>';
-} finally {//ce bloc sera executer  quoi quil arrive
-    echo additionner(4,5);
+function error2exception($code ,$message ,$fichier, $ligne)
+{
+    //le code fait office de severite
+    throw  new MonException($message,0,$code,$fichier,$ligne);
 }
-
-
-
+set_error_handler('error2exception');
